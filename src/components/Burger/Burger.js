72fb917d5e67b68bfe7e7222c727b6ee15bdd988
agdavid <antonio.g.david@gmail.com>
2018-09-30 21:00:52 -0400

    check if any ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -10,7 +10,16 @@ const burger = (props) => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />
             });
-        });
+        })
+        // flatten array to check if any ingredients
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    
+        if (transformedIngredients.length === 0) {
+            transformedIngredients = <p>Please start adding ingredients</p>
+        }
+    console.log(transformedIngredients);
 
     return(
         <div className={classes.Burger}>
