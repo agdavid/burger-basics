ad031298215533731024bec000b479b290ff86d8
agdavid <antonio.g.david@gmail.com>
2018-10-03 08:25:30 -0400

    add Backdrop that disappears on click

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -84,6 +84,10 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: true})
     };
 
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false});
+    }
+
     render() {
         // {salad: 0,...,meat: 0}
         const disabledInfo = {
@@ -96,7 +100,9 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
-                <Modal show={this.state.purchasing}>
+                <Modal 
+                    show={this.state.purchasing}
+                    modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary ingredients={this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
