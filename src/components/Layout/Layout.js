438b26368b627f963a2ad0e6a0b6aa2eed356e1a
agdavid <antonio.g.david@gmail.com>
2018-10-07 18:25:23 -0400

    add click handler to show SideDrawer on click

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,17 +7,23 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 class Layout extends Component {
     state = {
         showSideDrawer: true,
-
     }
     
     sideDrawerClosedHandler = () => {
         this.setState({ showSideDrawer: false });
     }
 
+    sideDrawerToggleHander = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        });
+    }
+
     render () {
         return (
             <Aux>
-                <Toolbar />
+                <Toolbar 
+                    drawerToggleClicked={this.sideDrawerToggleHander}/>
                 <SideDrawer 
                 open={this.state.showSideDrawer}
                 closed={this.sideDrawerClosedHandler}/>
