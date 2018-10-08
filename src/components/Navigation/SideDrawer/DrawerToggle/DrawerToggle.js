5899ec1f8aaef3b06fd038859cadd4428493a478
agdavid <antonio.g.david@gmail.com>
2018-10-08 07:42:37 -0400

    style menu as burger

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import classes from './DrawerToggle.css';
 
 const drawerToggle = (props) => {
     return (
         <div
+            className={classes.DrawerToggle}
             onClick={props.clicked}>
-            MENU</div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
     );
 }
 
