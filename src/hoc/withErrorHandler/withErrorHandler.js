b3e97e8dfed59808e74eb387920971fc5f50007c
agdavid <antonio.g.david@gmail.com>
2018-10-15 08:20:02 -0400

    wrap BurgerBuilder with hoc withErrorHandler for checking axios success or error

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Modal from '../../components/UI/Modal/Modal';
 import Aux from '../Aux/Aux';
 
-const withErrorHandler = (WrappedComponent) => {
-    return (props) => {
-        return (
-            <Aux>
-                <Modal>
-                    Something didn't work!
-                </Modal>
-                <WrappedComponent {...props} />
-            </Aux>
-        );
+// create a hoc that wraps any component 
+// use axios interceptors
+// on request, clear any existing error for a new request 
+// on response, check for an error and display modal with error
+
+const withErrorHandler = (WrappedComponent, axios) => {
+    return class extends Component {
+
+        state = {
+            error: null,
+        }
+
+        componentDidMount () {
+            axios.interceptors.request.use( request => {
+                this.setState({ error: null });
+                return request;
+            });
+            axios.interceptors.response.use(response => response, error => {
+                this.setState({ error: error });
+            });
+        }
+
+        errorConfirmedHandler () {
+            this.setState({ error: null });
+        }
+        
+        render () {
+            return (
+                <Aux>
+                    <Modal 
+                        show={this.state.error}
+                        modalClosed={this.errorConfirmedHandler}>
+                        {this.state.error ? this.state.error.message : null}
+                    </Modal>
+                    <WrappedComponent {...this.props} />
+                </Aux>
+            );
+        }
     }
+    
 }
 
 export default withErrorHandler;
