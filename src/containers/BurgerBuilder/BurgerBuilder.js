359df85a69e7203e9d6c5b9c51521f819430eb06
agdavid <antonio.g.david@gmail.com>
2018-11-20 07:40:55 -0500

    pass ingredients via query params

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -123,9 +123,15 @@ class BurgerBuilder extends Component {
 
         // on click of continue 
         // switch the page to /checkout and push a new page onto stack of pages
-        this.props.history.push(
-            '/checkout'
-        );
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+        }
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname:'/checkout',
+            search: `?${queryString}`,
+        });
     }
 
     render () {
