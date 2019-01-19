bf1fc3337b623e293d605778297cb44a22b02806
agdavid <antonio.g.david@gmail.com>
2019-01-19 08:16:55 -0500

    custom Input element

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,7 @@ import axios from '../../../axios-orders';
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
@@ -47,26 +48,22 @@ class ContactData extends Component {
     render () {
         let form = (
                 <form>
-                    <input
-                        className={classes.Input}
+                    <Input inputtype="input"
                         type="text"
                         name="name"
                         placeholder="Your name..."
                     />
-                    <input
-                        className={classes.Input}
+                    <Input inputtype="input"
                         type="email"
                         name="email"
                         placeholder="Your email..."
                     />
-                    <input
-                        className={classes.Input}
+                    <Input inputtype="input"
                         type="text"
                         name="street"
                         placeholder="Your street..."
                     />
-                    <input
-                        className={classes.Input}
+                    <Input inputtype="input"
                         type="text"
                         name="postalCode"
                         placeholder="Your postal code..."
